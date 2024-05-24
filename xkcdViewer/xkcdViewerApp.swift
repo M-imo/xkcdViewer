73ea52555c8b81a2790a2843e033989383310786
M-imo <97694145+M-imo@users.noreply.github.com>
2024-05-24 11:43:20 +0200

    Initial Commit

---
+++ xkcdViewer/xkcdViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  xkcdViewerApp.swift
+//  xkcdViewer
+//
+//  Created by Miriam Moe on 24/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct xkcdViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
